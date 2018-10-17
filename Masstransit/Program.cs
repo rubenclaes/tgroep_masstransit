1c0df6bd8fdb93ac602d3c0e8959562069d28c68
Ruben Claes <ruben.claes@euri.com>
2018-10-17 09:29:35 +0200

    Initial commit

---
+++ Masstransit/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace Masstransit
+{
+    public class YourMessage { public string Text { get; set; } }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                sbc.ReceiveEndpoint(host, "test_queue", ep =>
+                {
+                    ep.Handler<YourMessage>(context =>
+                    {
+                        return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+                    });
+                });
+            });
+
+            bus.Start(); // This is important!
+
+            bus.Publish(new YourMessage { Text = "Hi" });
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+    }
+    
+}
